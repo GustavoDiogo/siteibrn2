3515a0c31d1e3ce8b2e76cdcc274eb13ef9ec321
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-06-24 17:26:57 -0300

    Primeiros arquivos

---
+++ pages/footer.php
@@ -0,0 +1,60 @@
+<style type="text/css">
+		#ft-1 img{
+			width: 75px;
+		}
+	@media (max-width: 1199px){
+		#ft-3{			
+			margin: 0 auto;
+		}
+	}
+	@media (max-width: 991px){
+		#ft-1{
+			margin: 0 auto;			
+		}
+		#ft-1 img{
+			width: 100px;
+		}
+	}
+</style>
+
+<footer style="background-color: #ff732e">
+  <div style="background-color: #ff500e">
+    <div style="display: flex;flex-wrap: wrap" class="container">
+    	<div id="ft-1" align="left" style="padding: 1em 1em 1em 1em">
+    		<img src="images/ibrn-logo-rodape-transparente.png">
+    	</div>
+    	<div id="ft-2" align="center" style="padding: 1em 1em 1em 1em;color: white">
+    		RUA SILVIA DE BARROS, 125 &#8226; PEDREIRA &#8226; CEP 04462-010 &#8226; SÃO PAULO,SP &#8226; <i class="fa fa-phone"></i>(11) 5611-6811    		
+    	</div>
+        <div id="ft-3" style="padding: 1em 1em 1em 1em">
+          <a class="fb-ic">
+            <i class="fa fa-facebook white-text mr-4"> </i>
+          </a>
+          
+          <a class="tw-ic">
+            <i class="fa fa-twitter white-text mr-4"> </i>
+          </a>
+          
+          <a class="gplus-ic">
+            <i class="fa fa-google-plus white-text mr-4"> </i>
+          </a>
+          
+          <a class="li-ic">
+            <i class="fa fa-linkedin white-text mr-4"> </i>
+          </a>
+          
+          <a class="ins-ic">
+            <i class="fa fa-instagram white-text"> </i>
+          </a>
+
+        </div>        
+      </div> 
+    </div>
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<title>IB Represa Nova</title>
+
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #ff732e">
+		<a class="navbar-brand" href="" style="padding-left: 2em;padding-right: 2em"><img style="width: 200px;" src="images/ibrn-logo-transparente.png"></a>
+		<button id="navbutton" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<style type="text/css">
+			.nav-item{
+				
+				font-weight: 500;
+			}
+		</style>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<div style="align-content: middle">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a style="color: white" class="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">HOME</a>
+					</li>	
+					<li class="nav-item">
+						<a style="color: white" class="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">A IGREJA</a>
+					</li>
+					<li class="nav-item">
+						<a style="color: white" class="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">ESCOLA BÍBLICA</a>
+					</li>
+					<li class="nav-item">
+						<a style="color: white" class="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">DEVOCIONAIS</a>
+					</li>
+					<li class="nav-item">
+						<a style="color: white" class="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">CULTOS</a>
+					</li>
+				    <li class="nav-item dropdown">
+				        <a style="color: white" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				         MINISTÉRIOS
+				        </a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				          <a class="dropdown-item" href="#">JOVENS</a>
+				          <a class="dropdown-item" href="#">HOMENS</a>
+				          <a class="dropdown-item" href="#">MULHERES</a>
+				          <a class="dropdown-item" href="#">LOUVOR</a>
+				          <a class="dropdown-item" href="#">MISSÕES</a>
+				          <a class="dropdown-item" href="#">REPRESART</a>
+				          <a class="dropdown-item" href="#">REPRESA KIDS</a>
+				        </div>
+				    </li>			
+					<li class="nav-item">
+						<a style="color: white" class="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">REPRESART</a>
+					</li>
+					<li class="nav-item">
+						<a style="color: white" class="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">CONTATO</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+</head>
+<div style="padding-bottom: 6.6em"></div>
