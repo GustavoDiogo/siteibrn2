d1baa63df3e55dfd5b5f99042ff0914b2dfa0c9a
Gustavo Diogo <gustavo_guga_01@hotmail.com>
2018-07-21 14:14:54 -0300

    Mudanças da reuniao

--- footer.php
+++ footer.php
@@ -1,11 +1,27 @@
 <style type="text/css">
-		#ft-1 img{
-			width: 75px;
-		}
-    #ft-3 a{
+    #ft-1{
+      padding: 0.5em 0.5em 0.5em 0.5em;
+    }
+    #ft-1 img{
+      width: 75px;
+    }
+    #ft-2{
+      padding: 1.5em 0.5em 0.5em 0.5em;
       color: white;
     }
+    #ft-3{
+      position: relative;
+      float: right;
+      padding: 1.5em 0em 0em 0em;
+    }
+    #ft-3 a{
+      color: white;
+      padding: 0.75em 0.75em 0.75em 0.75em;
+    }
 	@media (max-width: 1199px){
+    #ft-2{
+      margin-top: 2em;
+    }
 		#ft-3{			
 			margin: 0 auto;
 		}
@@ -15,10 +31,14 @@
 			margin: 0 auto;			
 		}
 		#ft-1 img{
-			width: 100px;
+			width: 80px;
 		}
     #ft-2{
       font-size: 13px;
+      padding: 0em 0em 0em 0em;
+    }
+    #ft-3{
+      padding: 0em 0em 0em 0em;
     }
 	}
 </style>
@@ -26,21 +46,21 @@
 <footer style="background-color: #ff732e">
   <div style="background-color: #ff500e">
     <div style="display: flex;flex-wrap: wrap" class="container">
-    	<div id="ft-1" align="left" style="padding: 1em 1em 1em 1em">
+    	<div id="ft-1" align="left">
     		<img src="images/ibrn-logo-rodape-transparente.png">
     	</div>
-    	<div id="ft-2" align="center" style="padding: 1em 1em 1em 1em;color: white">
+    	<div id="ft-2" align="center">
     		RUA SILVIA DE BARROS, 125 &#8226; PEDREIRA &#8226; CEP 04462-010 &#8226; SÃO PAULO,SP &#8226; <i class="fa fa-phone"></i>(11) 5611-6811    		
     	</div>
-        <div id="ft-3" style="padding: 1em 1em 1em 1em">
+        <div id="ft-3">
           <a href="https://www.facebook.com/IBRepresaNova/" class="fb-ic">
-            <i class="fa fa-facebook white-text mr-4"> </i>
+            <i class="fa fa-facebook white-text"> </i>
           </a>
           <a href="https://www.instagram.com/ibrepresanova/" class="ins-ic">
-            <i class="fa fa-instagram white-text mr-4"> </i>
+            <i class="fa fa-instagram white-text"> </i>
           </a>
           <a href="https://www.youtube.com/channel/UCvNtPmmcjh_2N4hZYZruawA">
-            <i class="fa fa-youtube white-text mr-4"></i>
+            <i class="fa fa-youtube white-text"></i>
           </a>
         </div>        
       </div> 
--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <style type="text/css">
   #indexcarousel{
     width: 65%;
-    margin: 1em 0em 1em 1em;
+    margin: 0.5em 0em 0.5em 0.5em;
   }
 
   #devocional{
-    background-color: #3f35ff;
+    background-color: #087a8a;
     width: 35%;
-    margin: 1em 1em 1em 1em;
+    margin: 0.5em 0.5em 0.5em 0.5em;
   }
 
 @media (max-width: 991px){
@@ -18,7 +18,7 @@
   }
   #indexcarousel{
     width: 100%;
-    margin: 1em 1em 1em 1em;
+    margin: 0.5em 0.5em 0.5em 0.5em;
   }
   #devocional{
     width: 100%;
@@ -51,16 +51,13 @@
     </a>
   </div>
   <div id="devocional">
-    <div style="margin: 1em 1em 1em 1em;text-align: justify;">
+    <div style="margin: 1em 1em 1em 1em;text-align: justify;color: white">
       <h2 align="middle">Devocional da Semana</h2>
       Pensamento: Muitas vezes uma oportunidade nova parece boa aos nossos olhos e a impressão que temos é que tudo vai acabar bem, o problema é que as vezes nossa motivação está errada, e podemos acabar nos afastando da vontade de Deus. Por isso devemos sempre consultar ao Senhor e quando tivermos a resposta certa de Deus, então nossos planos passam a ser os mesmos planos de Deus, e assim podemos descansar, pois tudo o que Deus faz é perfeito e nunca falha.
       
       Oração: Pai querido, ajuda-me a lembrar sempre que eu devo consultar ao Senhor para que os meus caminhos sejam sempre os Seus caminhos, pois não quero andar fora do centro da sua vontade.
     </div>
   </div>
-</div>
-<div>
-  <object data="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.3524522291773!2d-46.6733364850196!3d-23.699104584614595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4f8a1d5bbd25%3A0xd2ad7e6684204df9!2sIgreja+Batista+Represa+Nova!5e0!3m2!1spt-BR!2sbr!4v1531700730424" frameborder="0" style="border:0;max-width: 100%;height: 500px;width: 500px" allowfullscreen></object>
 </div>
   
 <?php include("footer.php"); ?>
